d23083729f088535533a152b8b1de09f7ffb5b5e
hcimen <haruncimen@gmail.com>
2023-03-08 20:03:23 -0500

    updates

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("headerDropDown");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
