d5a6164ad1cb599bdbf3f2869456909945f7e2f9
hcimen <haruncimen@gmail.com>
2023-06-15 21:38:45 -0400

    change res.redirect

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -17,7 +17,7 @@ module.exports = {
     if (req.isAuthenticated()) {
     res.render('pages/addTransaction', { isAuthenticated: req.isAuthenticated() });
     }else{
-      res.redirect("/user/register")
+      res.render('pages/addTransaction')
     }
   },
 
@@ -67,7 +67,7 @@ module.exports = {
         }
       });
     } else {
-      res.redirect('/user/register');
+      res.render('pages/transactions');
     }
   },
 
